9305ee7e9d9dfc28d1b96a3ae0ebbbe2d9359501
arth2222 <71151788+arth2222@users.noreply.github.com>
2020-09-12 17:10:49 +0200

    sdsd

---
+++ HighScores/HighScores/Default.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="HighScores.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Database webforms</title>
+    <link href="~/styles/style.css" rel="stylesheet" />
+</head>
+<body>
+    <form runat="server">
+        <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+    </form>
+</body>
+</html>
---
+++ HighScores/HighScores/Default.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HighScores
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            InsertToDB();
+            BindData();
+        }
+
+        private void BindData()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["hsconnstrLocalDB"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("select * from HS",conn);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            GridView1.DataSource = reader;
+            GridView1.DataBind();
+            conn.Close();
+        }
+
+        private void InsertToDB()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["hsconnstrLocalDB"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+
+            int score = 1;
+            string username = "Kjell";
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO HS (score,username) values (@score,@username)", conn);
+
+            cmd.Parameters.AddWithValue("@score", score);
+            cmd.Parameters.AddWithValue("@username", username);
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
